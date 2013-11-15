0648a2d124375f9119a868a0748e877408f59614
Kondrashov Ilia <caseycs@gmail.com>
2013-11-15 12:08:24 +0300

    remove unused var

--- src/RedisPinba/RedisPinba.php
+++ src/RedisPinba/RedisPinba.php
@@ -13,7 +13,6 @@ class RedisPinba
     public function __call($method, $args)
     {
         $tags = array('group' => 'redis', 'op' => $method);
-        $data = array('args' => json_encode($args));
         $timer = pinba_timer_start($tags);
         $result = call_user_func_array(array($this->Redis, $method), $args);
         pinba_timer_stop($timer);
